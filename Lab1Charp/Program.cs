9d74e283a0b0fea7250d021cca6ed18f111871a4
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-02-07 22:04:58 +0200

    Task 1

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -2,15 +2,21 @@
 
  static void task1()
 {
-    Console.WriteLine("Task1 !");
-    Console.Write("s= ");
-    string? str = Console.ReadLine();
-    float s = 0; 
-    if (str != null) s = float.Parse(str);
-    double p = 4 * Math.Sqrt(s);
-    Console.WriteLine("p=" + p);
+    Console.WriteLine("          Task 1");
+
+    Console.Write("Input side a: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    double radius = (a / (2 * Math.Sqrt(3)));
+    Console.WriteLine();
+    Console.WriteLine("Side a: " + a);
+    Console.Write("Radius: " + radius);
+}
+
+static void task2()
+{ 
 
 }
-Console.WriteLine("Lab 1 !");
+Console.WriteLine("          Lab 1");
 task1();
+task2();
 // continue ...
