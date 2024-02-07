719f8dc99bc8514d8affb2fdfe9206f52898c118
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-02-07 22:13:39 +0200

    Task 2

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -10,13 +10,26 @@
     Console.WriteLine();
     Console.WriteLine("Side a: " + a);
     Console.Write("Radius: " + radius);
+    Console.WriteLine();
 }
 
 static void task2()
-{ 
+{
+    Console.WriteLine("          Task 2");
+    Console.Write("Input side a: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Input side b: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Input side c: ");
+    double c = Convert.ToDouble(Console.ReadLine());
 
+    if (a + b > c && a + c > b && b + c > a)
+        Console.WriteLine("Triangle exists");
+    else
+        Console.WriteLine("Triangle not exists");
+    Console.WriteLine();
 }
 Console.WriteLine("          Lab 1");
-task1();
+//task1();
 task2();
 // continue ...
